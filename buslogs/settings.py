c93ef2fe293cfc1f6a0046ce12c8efe990f7c763
Ajay2122 <kapuriyaajay21@gmail.com>
2026-01-01 00:30:22 +0530

    update vercel domain

--- buslogs/settings.py
+++ buslogs/settings.py
@@ -137,7 +137,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "driver-guide-fe.vercel.app"
+    "driver-guide-fe.vercel.app",
+    "https://driver-guide-fe.vercel.app"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
