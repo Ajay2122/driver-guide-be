56c073c92961b617241e04aef1b10d30fe6e91d7
Ajay2122 <kapuriyaajay21@gmail.com>
2026-01-01 00:10:09 +0530

    Add fe url to cors

--- buslogs/settings.py
+++ buslogs/settings.py
@@ -136,6 +136,7 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "driver-guide-fe.vercel.app"
 ]
 CSRF_TRUSTED_ORIGINS = [
     "http://localhost:3000",
